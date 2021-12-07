b3e760382d4ab5fda39556e65098422cefc353c6
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-12-07 20:59:15 +0200

    Update CustomerDataAccess.py

--- CustomerDataAccess.py
+++ CustomerDataAccess.py
@@ -5,11 +5,12 @@ from Customer import Customer
 class CustomerDataAccess:
 
     def __init__(self, file_path):
-        self.cursor = sqlite3.connect(file_path).cursor()
+        con = sqlite3.connect(file_path)
+        self.cursor = con.cursor()
 
     def print_all_customers(self):
-        self.cursor.execute("select * from Customer")
-        for row in self.cur:
+        self.cursor.execute("select * from customer")
+        for row in self.cursor:
             print(row)
 
     def insert_customer(self, customer):
@@ -18,18 +19,21 @@ class CustomerDataAccess:
                     f' "{customer.mobile}, {customer.address}")')
 
     def get_customer_by_id(self, id):
-        self.cursor.execute("select * from customers where "+
+        self.cursor.execute("select * from customer where "+
                          f" id = {id}")
         customer = None
-        for row in self.cur:
+        for row in self.cursor:
             customer = Customer(row[0], row[1], row[2], row[3], row[4])
         return customer
 
     def delete_customer(self, id):
-        self.cursor.execute("delete * from customers where " +
+        self.cursor.execute("delete from customer where " +
                             f" id = {id}")
 
     def update_customer(self, id, customer):
         self.cursor.execute(f'UPDATE customer SET ({customer.id}, ' +
                             f' "{customer.fname}", "{customer.lname}",' +
                             f' "{customer.mobile}, {customer.address}") where id = {id}')
+
+db = r'C:\sqlite\customer.db'
+a = CustomerDataAccess(db)
