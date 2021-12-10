17c8b1b703a066c4b06ae709f9a0a32f8de9d279
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-12-10 17:54:42 +0200

    Add files via upload

--- CustomerDataAccess.py
+++ CustomerDataAccess.py
@@ -14,7 +14,7 @@ class CustomerDataAccess:
             print(row)
 
     def insert_customer(self, customer):
-        self.cursor.execute(f'INSERT INTO customer VALUES ({customer.id}, ' +
+        self.cursor.execute(f'INSERT INTO customer VALUES ("{customer.id}", ' +
                     f' "{customer.fname}", "{customer.lname}",' +
                     f' "{customer.mobile}", "{customer.address}")')
         self.con.commit()
@@ -33,16 +33,18 @@ class CustomerDataAccess:
         self.con.commit()
 
     def update_customer(self, id, customer):
-        self.cursor.execute(f'UPDATE customer SET ("{customer.id}", ' +
-                            f' "{customer.fname}", "{customer.lname}",' +
-                            f' "{customer.mobile}", "{customer.address}") WHERE id = {id}' )
+        self.cursor.execute(f'UPDATE customer SET  id = {customer.id}, ' +
+                            f' fname = "{customer.fname}", lname = "{customer.lname}",' +
+                            f' mobile = "{customer.mobile}", address = "{customer.address}" where id = {id} ' )
         self.con.commit()
 
 db = r'C:\sqlite\customer.db'
 a = CustomerDataAccess(db)
-b = Customer(3, 'Bell', 'Harw', 'California', 6576)
+b = Customer(9, 'Bell', 'Harw', 'California', 6576)
 
 #print(a.insert_customer(b))
 #print(a.get_customer_by_id(2))
 #print(a.delete_customer(3))
+#print(a.update_customer(4,b))
 #print(a.print_all_customers())
+#rint(a.update_customer(4,b))
